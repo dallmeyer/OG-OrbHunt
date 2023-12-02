d8405a84ea44658d57b6e0f2aa702445a05696a9
dallmeyer <2515356+dallmeyer@users.noreply.github.com>
2023-12-01 17:41:31 -0800

    actually all moved over?

--- custom_levels/jak2/orbs-default-village1/orbs-default-village1.gd
+++ custom_levels/jak2/orbs-default-village1/orbs-default-village1.gd
@@ -2,7 +2,7 @@
 ;; We use the convention of having a longer DGO name for levels without precomputed visibility.
 
 ;; the actual file name still needs to be 8.3
-("O01.DGO"
+("O1H.DGO"
  (
   "orbs-default-village1.go"
   ))
