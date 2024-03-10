1773b51fffdaf56cbb0c0ca45c058d563c1eca8d
Matt Dallmeyer <mattdallmeyer@gmail.com>
2024-03-10 12:59:37 -0700

    finish dig? fix up drill dgo stuff

--- custom_levels/jak2/orbs-drill-drillb/orbs-drill-drillb.gd
+++ custom_levels/jak2/orbs-drill-drillb/orbs-drill-drillb.gd
@@ -2,7 +2,7 @@
 ;; We use the convention of having a longer DGO name for levels without precomputed visibility.
 
 ;; the actual file name still needs to be 8.3
-("O10.DGO"
+("O11.DGO"
  (
   "orbs-drill-drillb.go"
   ))
--- custom_levels/jak2/orbs-drill-drillmid/orbs-drill-drillmid.gd
+++ custom_levels/jak2/orbs-drill-drillmid/orbs-drill-drillmid.gd
@@ -2,7 +2,7 @@
 ;; We use the convention of having a longer DGO name for levels without precomputed visibility.
 
 ;; the actual file name still needs to be 8.3
-("O11.DGO"
+("O10.DGO"
  (
   "orbs-drill-drillmid.go"
   ))
