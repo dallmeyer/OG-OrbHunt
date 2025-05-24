bd5ca32d3b2afdd662d75eb51fd3f357f834d692
Matt Dallmeyer <mpdmeyer@vt.edu>
2025-05-23 17:11:32 -0700

    pre-sled from rublcst to railb

--- custom_assets/jak3/levels/orbs-rublcst/orbs-rublcst.gd
+++ custom_assets/jak3/levels/orbs-rublcst/orbs-rublcst.gd
@@ -4,5 +4,20 @@
 ;; the actual file name still needs to be 8.3
 ("O05.DGO"
  (
+  "vehicle-h.o"
+  "vehicle-part.o"
+  "vehicle-effects.o"
+  "vehicle.o"
+  "vehicle-util.o"
+  "vehicle-physics.o"
+  "vehicle-states.o"
+  "vehicle-manager.o"
+  "vehicle-hud.o"
+  "turret-control.o"
+  "target-pilot.o"
+  "pilot-states.o"
+  "h-sled.o"
+;;   "comb-rail-rider-ag.go" ;; assume rublcst (rbct) already has this
+  "warp-gate-ag.go"
   "orbs-rublcst.go"
   ))
