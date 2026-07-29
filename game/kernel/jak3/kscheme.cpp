aee2717eb36d162f17a785fe071d8b6ac6c6f198
Matt Dallmeyer <mpdmeyer@vt.edu>
2026-07-29 13:40:48 -0700

    facb with turrets n shit

--- game/kernel/jak3/kscheme.cpp
+++ game/kernel/jak3/kscheme.cpp
@@ -937,7 +937,7 @@ Ptr<Type> intern_type_from_c(int a, int b, const char* name, u64 methods) {
     auto type = alloc_and_init_type(casted_sym, n_methods, 0);  // allow level types
     type->symbol = casted_sym;
     type->num_methods = n_methods;
-    // MsgWarn("dkernel: defined type %s\n", name);
+    // MsgWarn("dkernel: defined type %s with %d methods\n", name, n_methods);
     return type;
   } else {
     // the type exists.
